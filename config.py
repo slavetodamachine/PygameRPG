f632dfa019a98ee92b14af9411e16a185486248f
slavetodamachine <whorchataxd@gmail.com>
2024-04-20 21:46:53 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,36 @@
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+TILESIZE = 32
+FPS = 60
+
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+
+PLAYER_SPEED = 3
+SPRINT_SPEED = 2
+ENEMY_SPEED = 2
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..E...............B',
+    'B....BBB...........B',
+    'B..................B',
+    'B.......P........B.B',
+    'B................B.B',
+    'B............BBBB..B',
+    'B....BBB...........B',
+    'B......B...........B',
+    'B......B...........B',
+    'B............E.....B',
+    'BBB..........B.....B',
+    'B............B.....B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
